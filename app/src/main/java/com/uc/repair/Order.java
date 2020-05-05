c22d9bbe3ce664c227bbb29e7732413bd724ac1a
ThePurpleVelvet <44989781+Xenesq@users.noreply.github.com>
2020-05-05 20:30:39 +0700

    Order

--- app/src/main/java/com/uc/repair/Order.java
+++ app/src/main/java/com/uc/repair/Order.java
@@ -2,13 +2,32 @@ package com.uc.repair;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Order extends AppCompatActivity {
+
+    Button buttonHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        buttonHome = findViewById(R.id.buttonHome);
+
+        Intent intent = getIntent();
+
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Order.this, Home.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
